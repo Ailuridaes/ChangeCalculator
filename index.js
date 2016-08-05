bdf4130261ab911e4226719f29a725de9c8b14a5
Ailuridaes <ailuridaes@gmail.com>
2016-08-04 17:06:50 -0700

    hides/shows divs, added input validation

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 $(function(){
+
+
   $("#calculate").click(function() {
+    //reset page elements
+    $(".changediv").hide();
+    $("#error").hide();
+
     var change = $("#received").val() - $("#saleTotal").val();
+
+    //input validation
+    if ( $("#saleTotal").val() === "" || isNaN($("#saleTotal").val()) || (change < 0) ) {
+      $("#error").show();
+      return;
+    }
 
     $("#dollars").text(Math.floor(change));
     change = Math.round(change % 1 * 100);
@@ -10,5 +22,7 @@ $(function(){
     change %= 10;
     $("#nickels").text(Math.floor(change/5));
     $("#pennies").text(Math.floor(change % 5));
+
+    $(".changediv").show();
   });
 });
